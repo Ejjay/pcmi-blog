ae5e885a4dc7de9f845288cadf0c407752b63e92
ejjays <christsonalloso021@gmail.com>
2025-04-14 11:15:24 +0800

    Auto commit

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,9 +1,10 @@
-import express from "express"
-import { getUserSavedPosts, savePost } from "../controllers/user.controller.js"
+import express from "express";
+import { createUser, getUserSavedPosts, savePost } from "../controllers/user.controller.js";
 
-const router = express.Router()
+const router = express.Router();
 
-router.get("/saved", getUserSavedPosts)
-router.patch("/save", savePost)
+router.post("/", createUser);
+router.get("/saved", getUserSavedPosts);
+router.patch("/save", savePost);
 
-export default router 
+export default router;
